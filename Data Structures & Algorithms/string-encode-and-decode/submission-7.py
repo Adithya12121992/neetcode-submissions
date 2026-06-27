ddc39df424816885556d4dfa41c97d9e7b5deb1b
Adithya <43809122+Adithya12121992@users.noreply.github.com>
2026-06-27 14:39:14 -0700

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return "__EMPTY_LIST__"
+        ret = []
+        for elem in strs:
+            if elem == "":
+                ret.append('E')
+                continue
+            each_ret = []
+            for i in range(len(elem)):
+                each_ret.append(str(ord(elem[i])))
+            ret.append(",".join(each_ret))
+        print(ret)
+        return "#".join(ret)
+
+    def decode(self, s: str) -> List[str]:
+        if s == "__EMPTY_LIST__":
+            return []
+        strings = s.split('#')
+        return_val = []
+        for each_str in strings:
+            if each_str == 'E':
+                return_val.append("")
+                continue
+            chars = each_str.split(',')
+            ret = ""
+            for char1 in chars:
+                ret+=chr(int(char1))
+            return_val.append(ret)
+        return return_val
+
