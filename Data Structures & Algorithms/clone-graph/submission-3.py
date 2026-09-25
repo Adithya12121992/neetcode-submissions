b24c3865d5c0bd61fb1cb3635261f602fa3cc81b
Adithya <43809122+Adithya12121992@users.noreply.github.com>
2026-09-25 06:04:41 -0700

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.py
@@ -0,0 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        '''if not node:
+            return 
+        cloned_graph = {}
+        cloned_graph[node] = Node(node.val)
+        q = deque([node])
+        seen = set()
+        while q:
+            popped_node = q.popleft()
+            for nei in popped_node.neighbors:
+                if nei not in cloned_graph:
+                    cloned_graph[nei] = Node(nei.val)
+                    q.append(nei)
+                cloned_graph[popped_node].neighbors.append(cloned_graph[nei])
+        return cloned_graph[node]'''
+        if not node:
+            return 
+        cloned_graph = {}
+        q = deque([node])
+        while q:
+            popped_node = q.popleft()
+            if popped_node not in cloned_graph:
+                cloned_graph[popped_node] = Node(popped_node.val)
+            for nei in popped_node.neighbors:
+                if nei not in cloned_graph:
+                    cloned_graph[nei] = Node(nei.val)
+                    q.append(nei)
+                cloned_graph[popped_node].neighbors.append(cloned_graph[nei])
+        return cloned_graph[node]
