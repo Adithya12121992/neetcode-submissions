5aa15cab9fe77c6c0859f30379d64633e4c40cd2
Adithya <43809122+Adithya12121992@users.noreply.github.com>
2026-09-26 06:32:43 -0700

    Add: find-the-celebrity - submission-6

---
+++ Data Structures & Algorithms/find-the-celebrity/submission-6.py
@@ -0,0 +1,26 @@
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+# def knows(a: int, b: int) -> bool:
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        truth_matrix = [False for _ in range(n)]
+
+        for i in range(n):
+            for j in range(n):
+                if i==j:
+                    continue
+                if knows(i, j):
+                    truth_matrix[i] = True # the person knows someone, check rule 1 
+                    break
+        # check if every one knows him , so chekc only the False rows
+        for i in range(n):
+            if not truth_matrix[i]:
+                is_celebrity= True
+                for j in range(n):
+                    if i==j:
+                        continue
+                    is_celebrity = is_celebrity and knows(j, i)
+                if is_celebrity:
+                    return i
+        return -1
